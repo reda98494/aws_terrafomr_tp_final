41baefc65cef47f51e96ac717ba47ee8631681c6
Reda <74600085+reda98494@users.noreply.github.com>
2022-06-03 12:55:07 +0200

    Create generate_inst.sh

---
+++ generate_inst.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+I=0
+
+while read line
+do
+	if [ $I -gt 0 ]
+	then
+		if [ ! -z $line ]
+		then
+			NAME=$(echo $line | cut -d";" -f1)
+			KEY_NAME="test_keypair"
+			AMI="ami-021d41cbdefc0c994"
+			SG_NAME=$(echo $line | cut -d";" -f2)
+			SUBNET=$(echo $line | cut -d";" -f3)
+			INSTANCE_TYPE=$(echo $line | cut -d";" -f4)
+			ASSOCIATE_PUBLIC_IP_ADDRESS=$(echo $line | cut -d";" -f5)
+			FILE_SERVICE=$(echo $line | cut -d";" -f6)
+
+			cp instance_template new_instance
+
+			sed -i "s|<##NAME##>|${1}_${NAME}|g" new_instance
+			sed -i "s|<##KEY_NAME##>|${KEY_NAME}|g" new_instance
+			sed -i "s|<##AMI##>|${AMI}|g" new_instance
+			sed -i "s|<##SG_NAME##>|${1}_${SG_NAME}|g" new_instance
+			sed -i "s|<##SUBNET##>|${1}_${SUBNET}|g" new_instance
+			sed -i "s|<##INSTANCE_TYPE##>|${INSTANCE_TYPE}|g" new_instance
+			sed -i "s|<##ASSOCIATE_PUBLIC_IP_ADDRESS##>|${ASSOCIATE_PUBLIC_IP_ADDRESS}|g" new_instance
+			sed -i "s|<##FILE_SERVICE##>|${FILE_SERVICE}|g" new_instance
+		
+			cat new_instance >> ${NAME}.inst
+			echo "" >> ${NAME}.inst
+			rm new_instance
+			mv ${NAME}.inst GENERATED
+		fi
+	else
+		I=1
+	fi
+done < flow_matrix_instances
+cat GENERATED/*.inst >> ./instances.tf
+rm GENERATED/*.inst
+
